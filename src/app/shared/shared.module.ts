b09e5a6d9ed4f1ea2f347d73eb8b52ec83fe8850
alexandra.conea4@yahoo.com <alexandra.conea4@yahoo.com>
2018-12-09 00:36:55 +0200

    added jobs browsing and job service

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { MatFormFieldModule, MatHint } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
 
 @NgModule({
   imports: 
